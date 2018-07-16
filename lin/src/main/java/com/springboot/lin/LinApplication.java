02b6adb3f388cfc0a3f9d61696c546bb286bc303
menglin319 <33089669+menglin319@users.noreply.github.com>
2018-07-16 15:26:31 +0800

    提交项目

---
+++ lin/src/main/java/com/springboot/lin/LinApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.lin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinApplication.class, args);
+	}
+}
